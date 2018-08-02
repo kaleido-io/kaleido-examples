0646ccc24672e0aba4892facb6a3f4ce1a419b25
Dylan <dylanmbryan@gmail.com>
2018-08-02 15:09:36 -0400

    Restructure dirs for geth/quorum; added quorum http example

---
+++ c#/nethereum/quorum/Application/Program.cs
@@ -0,0 +1,45 @@
+﻿using Nethereum.JsonRpc.Client;
+using Nethereum.Web3;
+using Nethereum.Quorum;
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class Program
+    {
+        // Fill these in to test
+        static string RPC_ENDPOINT = "";
+        static string USER = "";
+        static string PASS = "";
+
+        static Nethereum.Hex.HexTypes.HexBigInteger latestBlockNumber;
+
+        static void Main(string[] args)
+        {
+            // Call an async function to set the latest block number and Wait on it to return
+            getLatestBlockNumber().Wait();
+
+            // Print the latest block number once the getLatestBlockNumber() function returns
+            System.Diagnostics.Debug.Write("Latest Block: #" + latestBlockNumber.Value + "\n");
+        }
+
+        static async Task getLatestBlockNumber()
+        {
+            // Encode App Credentials as <username>:<password>
+            var byteArray = Encoding.ASCII.GetBytes(USER + ":" + PASS);
+            AuthenticationHeaderValue auth = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            // Create the RPC Client to talk to the Kaleido RPC endpoint using web3
+            IClient client = new RpcClient(new Uri(RPC_ENDPOINT), auth, null, null, null);
+            var web3 = new Web3Quorum(client);
+
+            //Now we can test the connection by calling some basic function
+            // Get the latest block number in the chain
+            latestBlockNumber = await web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
+
+        }
+    }
+}
---
+++ c#/nethereum/quorum/README.md
@@ -0,0 +1,9 @@
+# Nethereum Example Application
+An example application on Kaleido using Nethereum. This demonstrates how to connect to a Kaleido node using basic auth and retrieves an account balance.
+
+## Steps
+1. In Application/Program.cs
+   + Replace the RPC hostname with your RPC hostname.
+   + Replace the username and password with your basic auth credentials.
+   + Replcae the address with the address of the account you want the balance for
+2. Run the application.
