a86d34fc1f541055d47eccda852597802fd325b9
Fulton Byrne <fulton.byrne@consensys.net>
2018-08-06 14:24:39 -0400

    feat: bash example with curl

---
+++ bash/http/example.sh
@@ -0,0 +1,8 @@
+#! env bash
+# See: https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_getblockbynumber
+
+PAYLOAD='{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", true],"id":1}'
+URL='@RPC_ENDPOINT@'
+USER='@USER@'
+PASS='@PASS@'
+curl -X POST -H 'Content-Type: application/json' "$URL" --user $USER:$PASS --data "$PAYLOAD"
