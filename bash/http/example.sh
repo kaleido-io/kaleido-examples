8e899ca045941241b19cfe743ebda2c2e2687077
Nick Gaski <nickgaski@gmail.com>
2018-08-07 15:35:03 -0400

    add interpreter to let script run as executable

--- bash/http/example.sh
+++ bash/http/example.sh
@@ -1,4 +1,4 @@
-#! env bash
+#!/bin/bash
 # See: https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_getblockbynumber
 
 PAYLOAD='{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", true],"id":1}'
