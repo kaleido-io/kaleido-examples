fa8e2061ba1c65434ffd9ff1c75479c466b71643
Dylan <dylanmbryan@gmail.com>
2018-08-09 14:04:29 -0400

    Added bash walkthrough gif; added comments and spacing to bash script

--- bash/http/example.sh
+++ bash/http/example.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # See: https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_getblockbynumber
 
-PAYLOAD='{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", true],"id":1}'
+# Fill these in to test, ex. remove @RPC_ENDPOINT@
 URL='@RPC_ENDPOINT@'
 USER='@USER@'
 PASS='@PASS@'
+
+# Get the latest block in the chain
+PAYLOAD='{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", true],"id":1}'
+
+# Curl the endpoint with the data payload and the user credentials
 curl -X POST -H 'Content-Type: application/json' "$URL" --user $USER:$PASS --data "$PAYLOAD"
