16a5ba61f49c0c0fad1dc4cb0a73903a5c6f2703
Vinod Damle <vdamle@users.noreply.github.com>
2022-06-01 14:02:45 -0400

    Configure https agent and bump web3 version (#19)
    * sets maxSockets and keepAlive on https agent and uses that when
      setting up web3 connection
    * bumps up web3 version to latest

--- javascript/web3js/wss/index.js
+++ javascript/web3js/wss/index.js
@@ -1,17 +1,20 @@
 const Web3 = require('web3');
+const https = require('https');
+
+const web3Agent = https.Agent({ keepAlive: true, maxSockets: 5 });
+const options = { agent: { https: web3Agent }};
 
 // Fill these in to test, ex. remove @WSS_ENDPOINT@
 let USER = "@USER@";
 let PASS = "@PASS@";
-let WSS_ENDPOINT = "@WSS_ENDPOINT_NO_PRE@"; // Remove the leading wss://
-
+let WSS_ENDPOINT = "@WSS_ENDPOINT_NO_PRE@"; // Remove the leading https://
 
 // Web Socket Example
 // NOTE: Basic Auth support for websockets was added in 1.0.0-beta.35, prior versions do not support basic auth
 
 let nodeUrl = "wss://" + USER + ":" + PASS + "@" + WSS_ENDPOINT;
 
-let provider = new Web3.providers.WebsocketProvider(nodeUrl);
+let provider = new Web3.providers.WebsocketProvider(nodeUrl, options);
 let web3 = new Web3(provider);
 
 // Now you can call web3 functions, so we'll just test the connection by getting the latest block in the chain.
